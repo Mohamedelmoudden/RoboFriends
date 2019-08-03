b7c8ed16fa0c336c81a8f7a45dea4232a1d40e2e
Mohamedelmoudden <mohamedelmoudden17@gmail.com>
2019-08-03 19:12:53 +0200

    more changes

--- src/CardList.js
+++ src/CardList.js
@@ -4,7 +4,7 @@ import Card from './Card';
 const CardList = ({ robots }) => {
 	return (
 		<div>
-		{
+		  {
 			robots.map((user, i) => {
 				return (
 					<Card 
@@ -12,9 +12,10 @@ const CardList = ({ robots }) => {
 					id={robots[i].id} 
 					name={robots[i].name} 
 					email={robots[i].email} 
-					/>)
-			})
-		 }
+		 			/>
+		 		);
+			  })
+		  }
 		</div>	
 	);
 }
